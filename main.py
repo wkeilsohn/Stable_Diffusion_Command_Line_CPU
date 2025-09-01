53af4a43637e5e35b24b4ce9bd14534f55cc2c58
William Keilsohn <kingwilliam@gmaail.com>
2025-09-01 14:35:48 +0900

    Updated Model Caller

--- main.py
+++ main.py
@@ -13,19 +13,26 @@ cpath = os.getcwd()
 mfolder = os.path.join(cpath, "models")
 dpath = os.path.join(cpath, "diffusers")
 torch_dtype = torch.float32
-device = "cpu"
+device = "cpu" # Assumed there is no GPU present.
 
 convert_str = "python ./diffusers/scripts/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path {} --dump_path {} {}"
 
 # Check for Models
+
+def model_folder_parter(): # Syntax needs adjustment.
+    try:
+        os.system("mkdir ./diffusers/models")
+    except:
+        pass
+
 def model_converter():
+    model_folder = os.path.join(dpath, "models")
     for i in os.scandir(mfolder):
         if i.is_file():
             md = os.path.basename(i)
             md_name = md.split(".")[0]
             ending = md.split(".")[1]
-            # print(md_name)
-            tmp_dir = os.path.join(dpath, md_name)
+            tmp_dir = os.path.join(model_folder, md_name)
             if os.path.isdir(tmp_dir):
                 pass
             else:
@@ -34,8 +41,17 @@ def model_converter():
                 else:
                     end_val = ""
                 md_local = os.path.join(mfolder, md)
-                cstring = convert_str.format(md_local, md_name, end_val)
+                cstring = convert_str.format(md_local, tmp_dir, end_val)
                 os.system(cstring)
+
+def getModels():
+    dirs = []
+    model_folder = os.path.join(dpath, "models")
+    for i in os.listdir(model_folder):
+      f = os.path.join(model_folder, i)
+      if os.path.isdir(f):
+            dirs.append(i)
+    return dirs
 
 # Declare Functions
 
@@ -44,4 +60,7 @@ def model_converter():
 
 # Run Application
 if __name__ == "__main__":
+    # model_folder_parter()
     model_converter()
+    models = getModels()
+    print(models)
