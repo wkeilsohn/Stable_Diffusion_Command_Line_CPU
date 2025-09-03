0786671416888fe38e3520242ca993aa409572fb
William Keilsohn <kingwilliam@gmaail.com>
2025-09-03 11:23:47 +0900

    Basic Console Commands

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 # Import Packages
 import os
+import argparse
 import time
 import torch
 from diffusers import StableDiffusionPipeline
@@ -31,20 +32,18 @@ def image_namer():
     ctime = time.strftime(fstring, ctime)
     return ctime + ".png"
 
-# Interface 
+# Central Functionality
 
-def start_program(timesteps, sigmas, guidence_scale, negative_prompt, num_images_per_prompt, eta, generator, latents, prompt_embeds, negative_prompt_embeds, ip_adapter_image, ip_adapter_image_embeds, output_type, return_dict, guidence_rescale, clip_skip, callback_on_step_end, callback_on_step_end_tensor_inputs, image_name="", gen_models=False, List_Models=False, Model="", prompt=prompt, height=height, width=width, num_inference_steps=50):
-    # runs the user interface inside main.
-    print("Hello World")
+def central_program(timesteps, sigmas, guidence_scale, negative_prompt, num_images_per_prompt, eta, generator, latents, prompt_embeds, negative_prompt_embeds, ip_adapter_image, ip_adapter_image_embeds, output_type, return_dict, guidence_rescale, clip_skip, callback_on_step_end, callback_on_step_end_tensor_inputs, image_name="", gen_models=False, List_Models=False, Model="", prompt=prompt, height=height, width=width, num_inference_steps=50):
     if gen_models == True:
         model_converter()
-    if List_Models == True:
+    elif List_Models == True:
         models = getModels()
         if len(models) == 0:
             print("There are no models available. Please download and convert models.")
         else:
             print(models)
-    if not Model: #Probably unnecessary to check again, but better safe than sorry.
+    elif not Model: #Probably unnecessary to check again, but better safe than sorry.
         pipe = diffusion_wrapper(model=Model)
         image = image_wrapper(pipe=pipe, height=height, width=width, num_inference_steps=num_inference_steps, timesteps=timesteps, sigmas=sigmas, guidence_scale=guidence_scale, negative_prompt=negative_prompt, num_images_per_prompt=num_images_per_prompt, eta=eta, generator=generator, latents=latents, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, ip_adapter_image=ip_adapter_image, ip_adapter_image_embeds=ip_adapter_image_embeds, output_type=output_type, return_dict=return_dict, guidence_rescale=guidence_rescale, clip_skip=clip_skip, callback_on_step_end=callback_on_step_end, callback_on_step_end_tensor_inputs=callback_on_step_end_tensor_inputs, prompt=prompt)
         if not image_name:
@@ -52,8 +51,41 @@ def start_program(timesteps, sigmas, guidence_scale, negative_prompt, num_images
             image.save(name)
     else:
         print("No model selected. Please select a model and re-run application.")
-        pass
+        
+
+# Interface
+def parse_maker():
+    parser = argparse.ArgumentParser(description='Command Line Stable Diffusion Image Generation for Local Models')
+    parser.add_argument("--timesteps", required=False, default=None)
+    parser.add_argument("--sigmas", required=False, default=None)
+    parser.add_argument("--guidence_scale", required=False, default=None)
+    parser.add_argument("--negative_prompt", required=False, default=None)
+    parser.add_argument("--num_images_per_prompt", required=False, default=1)
+    parser.add_argument("--eta", required=False, default=None)
+    parser.add_argument("--generator", required=False, default=None)
+    parser.add_argument("--latents", required=False, default=None)
+    parser.add_argument("--prompt_embeds", required=False, default=None)
+    parser.add_argument("--negative_prompt_embeds", required=False, default=None)
+    parser.add_argument("--ip_adapter_image", required=False, default=None)
+    parser.add_argument("--ip_adapter_image_embeds", required=False, default=None)
+    parser.add_argument("--output_type", required=False, default=None)
+    parser.add_argument("--return_dict", required=False, default=None)
+    parser.add_argument("--guidence_rescale", required=False, default=None)
+    parser.add_argument("--clip_skip", required=False, default=None)
+    parser.add_argument("--callback_on_step_end", required=False, default=None)
+    parser.add_argument("--callback_on_step_end_tensor_inputs", required=False, default=None)
+    parser.add_argument("--image_name", required=False, default="", type=str)
+    parser.add_argument("--gen_models", required=False, default=False, type=bool)
+    parser.add_argument("--list_models", required=False, default=False, type=bool)
+    parser.add_argument("--model", required=False, default="", type=str)
+    parser.add_argument("--prompt", required=False, default=prompt, type=str)
+    parser.add_argument("--height", required=False, default=height, type=int)
+    parser.add_argument("--width", required=False, default=width, type=int)
+    parser.add_argument("--num_inference_steps", required=False, default=50, type=int)
+    args = parser.parse_args()
+    return args
 
 # Run Application
 if __name__ == "__main__":
-    start_program()
+    args = parse_maker()
+    central_program(timesteps=args.timesteps, sigmas=args.sigmas, guidence_scale=args.guidence_scale, negative_prompt=args.negative_prompt, num_images_per_prompt=args.num_images_per_prompt, eta=args.eta, generator=args.generator, latents=args.latents, prompt_embeds=args.prompt_embeds, negative_prompt_embeds=args.negative_prompt_embeds, ip_adapter_image=args.ip_adapter_image, ip_adapter_image_embeds=args.ip_adapter_image_embeds, output_type=args.output_type, return_dict=args.return_dict, guidence_rescale=args.guidence_rescale, clip_skip=args.clip_skip, callback_on_step_end=args.callback_on_step_end, callback_on_step_end_tensor_inputs=args.callback_on_step_end_tensor_inputs, image_name=args.image_name, gen_models=args.gen_models, List_Models=args.list_models, Model=args.model, prompt=args.prompt, height=args.height, width=args.width, num_inference_steps=args.num_inference_steps)
